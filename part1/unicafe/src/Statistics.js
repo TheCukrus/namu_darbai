26240dd84844726c127196b39b2d72ec5cd993c3
Jevgenij <sssgustavsss@gmail.com>
2022-03-17 19:57:35 +0200

    1.9

--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -2,22 +2,26 @@ import React from "react";
 
 const Statistics = (props) =>
 {
-const good = props.good;
-const neutral = props.neutral;
-const bad = props.bad;
+    const good = props.good;
+    const neutral = props.neutral;
+    const bad = props.bad;
 
-    return (
-        <div>
-            <h3>good {good}<br />
-                neutral {neutral} <br />
-                bad {bad}<br />
-                all {good + neutral + bad}<br />
-                average {(good - bad) / (good + neutral + bad)}<br />
-                positive {good / (good + neutral + bad) * 100}%
+    if ((good === 0) && (neutral === 0) && (bad === 0))
+    {
+        return <p>No feedback given</p>
+    }
+        return (
+            <div>
+                <h3>good {good}<br />
+                    neutral {neutral} <br />
+                    bad {bad}<br />
+                    all {good + neutral + bad}<br />
+                    average {(good - bad) / (good + neutral + bad)}<br />
+                    positive {good / (good + neutral + bad) * 100}%
 
-            </h3>
-        </div>
-    )
+                </h3>
+            </div>
+        )
 }
 
 export default Statistics;
