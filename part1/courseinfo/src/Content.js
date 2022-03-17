d1855f4314196272a190d01ebd1c5cd007f2d772
Jevgenij <sssgustavsss@gmail.com>
2022-03-17 14:47:23 +0200

    1.4

--- part1/courseinfo/src/Content.js
+++ part1/courseinfo/src/Content.js
@@ -5,9 +5,9 @@ const Content = (props) =>
 {
     return (
         <div>
-            <Part name1={props.part1.name} exercises1={props.part1.exercises} />
-            <Part name2={props.part2.name} exercises2={props.part2.exercises}/> 
-            <Part name3={props.part3.name} exercises3={props.part3.exercises}/>
+            <Part name1={props.parts[0].name} exercises1={props.parts[0].exercises} />
+            <Part name2={props.parts[1].name} exercises2={props.parts[1].exercises}/> 
+            <Part name3={props.parts[2].name} exercises3={props.parts[2].exercises}/>
         </div>
     )
 }
