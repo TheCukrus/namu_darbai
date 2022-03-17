4a22d96cb8f0341df468fb75feae6ffa94a2b432
Jevgenij <sssgustavsss@gmail.com>
2022-03-17 10:03:18 +0200

    1.1

---
+++ part1/courseinfo/src/Total.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Total = (props) =>
+{
+    return(
+        <div>
+      <p>Number of exercises {props.exercises}</p>
+        </div>
+    )
+}
+
+export default Total;
