73bc00a7ab17d1e30138fd6ad8c392116859514f
Jevgenij <sssgustavsss@gmail.com>
2022-03-23 19:52:41 +0200

    2.1*

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,31 +1,34 @@
-import Content from "./Content"
-import Header from "./Header"
-import Total from "./Total"
+import React from "react"
+import Course from "./Course"
 
 const App = () =>
 {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
+  
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total exercises={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} />
+      <Course course={course} />
+      
     </div>
   )
 }
---
+++ part1/courseinfo/src/Course.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Content from "./Content";
+import Total from "./Total";
+
+const Course = (props) =>
+{
+    const course = props.course;
+
+    return (
+        <div>
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total exercises={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} />
+        </div>
+    )
+}
+
+export default Course;
