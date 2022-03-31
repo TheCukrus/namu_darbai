f01b51ddb093c9497ab4bfdf93fa60915fcc9698
Jevgenij <sssgustavsss@gmail.com>
2022-03-31 13:14:20 +0300

    2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,10 +4,14 @@ const App = () =>
 {
   //useStates
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState()
+  const [filterNumber, setFilterNumber] = useState("")
 
   //functions
   const handle_submit = (event) =>
@@ -28,15 +32,29 @@ const App = () =>
 
   const handle_on_change = (event) => setNewName(event.target.value)
   const handle_on_number = (event) => setNewNumber(event.target.value)
+  const filter_numbers = (event) => setFilterNumber(event.target.value)
 
-  //renders numbers
-  const temp1 = persons.map((ele, i) => <p key={i}>{ele.name} {ele.number}</p>)
+  const temp1 = [];
+  const regex1 = new RegExp(filterNumber, "i");
+
+  for (let i = 0; i < persons.length; i++)
+  {
+    if (regex1.test(persons[i].name))
+    {
+      temp1.push(<p key={i}>{persons[i].name} {persons[i].number}</p>)
+    }
+  }
 
 
+  
   // console.log(persons)
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <p>filter shown with <input onChange={filter_numbers} /></p>
+
+      <h2>add a new</h2>
       <form onSubmit={handle_submit}>
         <div>
           name: <input onChange={handle_on_change} />
@@ -49,8 +67,8 @@ const App = () =>
         </div>
       </form>
       <h2>Numbers</h2>
+      {/* {persons.map(ele => <p key={ele.id}>{ele.name} {ele.number}</p>)} */}
       {temp1}
-
     </div>
   )
 }
